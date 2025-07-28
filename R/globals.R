d3bc6a22a30784e196ece12825d668f72949b283
gosukehommaEX <my.name.is.gosuke@gmail.com>
2025-07-28 17:01:46 +0900

    Delete the function, because the data handling package was changed from data.table to dplyr.

--- R/globals.R
+++
@@ -1,8 +0,0 @@
-#' @importFrom utils globalVariables
-#' @keywords internal
-globalVariables(c(
-  # data.table variables used in NSE
-  "simID", "total", "analysis", "dropout", ".", "event_rank", "is_event",
-  "analysis.time", "accrual.time", "tte", "combination_key", "intensity_val",
-  "proportion_val", "surv.time", "dropout.time", "group", "subgroup"
-))
